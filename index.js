bbbbf49c7a9861c445514305e43b48cee37b9a35
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-14 06:02:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-function add(){
-  var a = 1 
-  var b = 2
-  a + b 
-  return add
-}
-
-add();
-//look, I really don't know how to make these tests run, but here are the functions you are asking for. 
+var a,b 
 
